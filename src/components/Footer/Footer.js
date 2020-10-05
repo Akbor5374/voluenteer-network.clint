fa492d3a45af1230e9a9af94b8645337cf5966b3
Akbor5374 <akborhossain53741@gmail.com>
2020-10-05 23:54:44 +0600

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+         <h6 className="text-center bg-dark py-4 text-light">
+             Copyright © 2017 StrongLoop and volunteer.com contributors.
+          </h6>   
+        </>
+    );
+};
+
+export default Footer;
